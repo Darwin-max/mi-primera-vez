7823f15d8a77f42d92b68ebd001dd3a5c51dad8e
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2024-12-03 07:51:33 -0500

    TIPOS DE VARIABLE DOTOS PRIMITIVOS MODIFICAR DATOS

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("HELLO WORLD") 
+#variables y tipos de datos primitivos 
+#-string = cadenas de caracters "str"
+#-char = caracters 'int' desimal 'float'
+#-bolean = Datos logicos'bool'
+#- 
+name = "Darwin Samuel"
+age = 16 #numeros enteros
+heigth = 1.76 
+treiner = True
+ # CONJUNTO DE DATOS
+ # -array = conjuntos de datos de uba dimencion 'list' 
+hobbies = list()
+hobbies = ["cocinar" ,"programar"]
+#MODIFICAR UN DATO DE LA LISTA
+print(hobbies[0])
+dato = input("ingrese otro pasatiempo:")
+hobbies.append(dato)
+print(hobbies)
